150848e97ac508f710991f71344535b81b209d47
VaninaLopetegui <vaninalopetegui@gmail.com>
2023-09-29 17:28:16 -0300

    Se terminó y solucionó código con URL usando POST y fetch

--- js/main.js
+++ js/main.js
@@ -6,20 +6,22 @@ form.addEventListener("submit", (e) =>{
     const lastName = document.getElementById ("lastName").value;
     const birthDay = document.getElementById ("birthDay").value;
     datosUsuario.push(name, lastName, birthDay)
-    fetch("https://jsonplaceholder.typicode.com/users.json", {
+    datosUsuario.push(name, lastName, birthDay)
+    fetch("https://jsonplaceholder.typicode.com/users", {
         method: "POST",
-        body: JSON.stringify({
-            nombre: name,
-            apellido: lastName,
-            nacimiento: birthDay
-        })
+        headers: {'Content-Type': 'application/json'},
+        body: JSON.stringify(datosUsuario),
     })
-    .then(res => res.json())
+    .then(res => {
+        if(res.ok){
+        return res.json();
+    }})
     .then(data => {
         const name = document.getElementById("name").value= "";
         const lastName = document.getElementById ("lastName").value= "";
         const birthDay = document.getElementById ("birthDay").value= "";
         alert("Se envío los datos!");
+        console.log(data)
     })    
     .catch(error => console.log(error))
 })
